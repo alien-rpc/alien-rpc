2d8af2af2418f2c997dde0f684301df8a0092948
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-02-11 16:54:50 -0500

    test: few more tests for parsePathParams

--- packages/pathic/test/parsePathParams.test.ts
+++ packages/pathic/test/parsePathParams.test.ts
@@ -3,6 +3,7 @@ import { parsePathParams } from '../src/parsePathParams.js'
 describe('parsePathParams', () => {
   test('single named parameter', () => {
     expect(parsePathParams('/:foo')).toEqual(['foo'])
+    expect(parsePathParams('/:foo/bar')).toEqual(['foo'])
   })
   test('multiple named parameters', () => {
     expect(parsePathParams('/genre/:genre/book/:book')).toEqual([
@@ -15,5 +16,12 @@ describe('parsePathParams', () => {
     expect(parsePathParams('/user/*/edit')).toEqual(['*'])
     expect(parsePathParams('/user/*rest')).toEqual(['rest'])
     expect(parsePathParams('/user/*inner/edit')).toEqual(['inner'])
+  })
+  test('path with no parameters', () => {
+    expect(parsePathParams('/foo/bar')).toEqual([])
+    expect(parsePathParams('/')).toEqual([])
+  })
+  test('path with mixed named and wildcard parameters', () => {
+    expect(parsePathParams('/:foo/*')).toEqual(['foo', '*'])
   })
 })
